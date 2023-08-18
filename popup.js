e76dcdc6c17817b877c521d8cde6ba2c595c7328
Devansh <devansh0403@gmail.com>
2023-08-18 20:42:12 +0530

    test

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const screenshotButton = document.getElementById("screenshotButton");
-
+  // hey
   screenshotButton.addEventListener("click", function () {
     chrome.runtime.sendMessage(
       { action: "takeScreenshot" },
